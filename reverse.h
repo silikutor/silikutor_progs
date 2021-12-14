3095e37fd038f06d792cf7e29c21ed5daea9a94d
silikutor <47700896+silikutor@users.noreply.github.com>
2021-12-14 04:03:21 +0100

    Delete reverse.h

--- reverse.h
+++
@@ -1,9 +0,0 @@
-#ifndef REVERSE_H
-#define REVERSE_H
-
-void reverse(FILE *fp);
-char **read(FILE *fp, int *ptrLines);
-void write(char **lines, int lineNum);
-void memoryFail(void *pMem);
-
-#endif
